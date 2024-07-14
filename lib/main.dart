e162b1a1a7360ba5114fe3d527d29cb079f83b28
khuzaifa17 <Khuzaifaazam17@gmail.com>
2024-07-14 16:44:30 +0500

    message

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:weatherapp/Screens/Homepage.dart';
+
+void main() {
+  HttpOverrides.global = MyHttpOverrides();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
